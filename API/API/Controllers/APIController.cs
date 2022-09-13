8e0862d8a88631644dad5e0baf85e7b3e39d8e17
Dusan <dusannrt8619@gs.viser.edu.rs>
2022-09-13 23:21:53 +0200

    verzija_1

---
+++ API/API/Controllers/APIController.cs
@@ -0,0 +1,289 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using mojePreduzece.Models;
+using faktura.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace mojePreduzece.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class APIController : Controller
+    {
+        static List<Preduzece> preduzeca = new List<Preduzece>()
+            {
+             new Preduzece { ime = "Dusan", prezime = "Nikolic", email = "dusannrt8619@gs.viser.edu.rs", naziv = "Naziv1", adresa = "Bulevar Oslobodjenja 221", PIB = 111100000 },
+             new Preduzece { ime = "Marko", prezime = "Markic", email = "markicm@gmail.com", naziv = "Naziv2", adresa = "Niksicka 34", PIB = 111100001 },
+             new Preduzece { ime = "Ivan", prezime = "Ivic", email = "ivicivan@gamil.com", naziv = "Naziv3", adresa = "Miroslava Zetskog 11a", PIB = 111100002 },
+             new Preduzece { ime = "Petar", prezime = "Peric", email = "petarp@gamil.com", naziv = "Naziv4", adresa = "Kralja Filipa 17", PIB = 111100003 },
+             new Preduzece { ime = "Milos", prezime = "Misic", email = "milosm@gamil.com", naziv = "Naziv5", adresa = "Nova 14", PIB = 111100004 }
+           };
+        static List<Faktura> fakture = new List<Faktura>()
+            {
+             new Faktura { id = 1 ,PIBkome = 111100000, PIBodKoga = 111100001, datumGenerisanja =  new DateTime(2022, 9, 14), datumPlacanja = new DateTime(2022, 9, 14), ukupnaCena = 500, tipFakture = "izlazna",naziv= "faktura" ,cenaPoJediniciMere= 100 ,jedinicaMere= "RSD",kolicina = 5},
+             new Faktura { id = 1 ,PIBkome = 111100001, PIBodKoga = 111100000, datumGenerisanja =  new DateTime(2022, 9, 14), datumPlacanja = new DateTime(2022, 9, 14), ukupnaCena = 500, tipFakture = "ulazna",naziv= "faktura" ,cenaPoJediniciMere= 100 ,jedinicaMere= "RSD",kolicina = 5}
+           };
+        //ready
+        [HttpPost("unosFakture")]
+        public IActionResult NovaFakture([FromForm] int PIBkome, [FromForm] int PIBodKoga, [FromForm] DateTime datumPlacanja, [FromForm] decimal ukupnaCena, [FromForm] string tip, [FromForm] string naziv, [FromForm] decimal cenaPoJediniciMere, [FromForm] string jedinicaMere, [FromForm] int kolicina)
+        {
+            var odKoga = preduzeca.FirstOrDefault(obj => obj.PIB == PIBodKoga);
+            var kome = preduzeca.FirstOrDefault(obj => obj.PIB == PIBkome);
+            if (odKoga == null || kome == null || kome==odKoga)
+            {
+                return BadRequest("Neispravno pravljenje fakture");
+            }
+            string drugaFaktura = "";
+            if (tip == "ulazna")
+            {
+                drugaFaktura = "izlazna";
+            }
+            else
+            {
+                drugaFaktura = "ulazna";
+            }
+            int id= fakture.OrderByDescending(k => k.id).First().id + 1;
+            var trenutnoVreme = DateTime.Now;
+            Faktura faktura= new Faktura();
+            Faktura faktura2 = new Faktura();
+            faktura.id = id;
+            faktura.PIBkome = PIBkome;
+            faktura.PIBodKoga = PIBodKoga;
+            faktura.datumGenerisanja = trenutnoVreme;
+            faktura.datumPlacanja = datumPlacanja;
+            faktura.ukupnaCena = ukupnaCena;
+            faktura.tipFakture = tip;
+            faktura.naziv = naziv;
+            faktura.cenaPoJediniciMere = cenaPoJediniciMere;
+            faktura.jedinicaMere = jedinicaMere;
+            faktura.kolicina = kolicina;
+            faktura2.id = id;
+            faktura2.PIBkome = PIBodKoga;
+            faktura2.PIBodKoga = PIBkome;
+            faktura2.datumGenerisanja = trenutnoVreme;
+            faktura2.datumPlacanja = datumPlacanja;
+            faktura2.ukupnaCena = ukupnaCena;
+            faktura2.tipFakture = drugaFaktura;
+            faktura2.naziv = naziv;
+            faktura2.cenaPoJediniciMere = cenaPoJediniciMere;
+            faktura2.jedinicaMere = jedinicaMere;
+            faktura2.kolicina = kolicina;
+            fakture.Add(faktura);
+            fakture.Add(faktura2);
+            return Ok(fakture);
+        }
+        //ready
+        [HttpPost("izmeniFakturu/{id}/{PIB}")]
+        public IActionResult IzmeniFakturu([FromForm] int PIBkome, [FromForm] int PIBodKoga, [FromForm] DateTime datumPlacanja, [FromForm] decimal ukupnaCena, [FromForm] string tip, [FromForm] string naziv, [FromForm] decimal cenaPoJediniciMere, [FromForm] string jedinicaMere, [FromForm] int kolicina,int id,double PIB)
+        {
+            var prvaFaktura = fakture.FirstOrDefault(obj=>obj.PIBkome == PIB && obj.id==id);
+            var drugaFaktura = fakture.FirstOrDefault(obj => obj.PIBodKoga == PIB && obj.id == id);
+            string tipDrugeFakture = "";
+            if (prvaFaktura == null || drugaFaktura == null)
+            {
+                return BadRequest("Neuspela izmena. Proverite PIB-ove");
+            }
+            if (tip == "ulazna")
+            {
+                tipDrugeFakture = "izlazna";
+            }
+            else
+            {
+                tipDrugeFakture = "ulazna";
+            }
+
+            var PIBkomPreduzecu = preduzeca.FirstOrDefault(obj => obj.PIB == PIBkome);
+            var PIBodKogPreduzeca= preduzeca.FirstOrDefault(obj => obj.PIB == PIBodKoga);
+            if (PIBkomPreduzecu == null || PIBodKogPreduzeca == null || PIBkomPreduzecu == PIBodKogPreduzeca)
+            {
+                return BadRequest("Ne postoje preduzeca / PIB-ovi su isti");
+            }
+            prvaFaktura.PIBkome = PIBkome;
+            prvaFaktura.PIBodKoga = PIBodKoga;
+            prvaFaktura.datumGenerisanja = DateTime.Now;
+            prvaFaktura.datumPlacanja = datumPlacanja;
+            prvaFaktura.ukupnaCena = ukupnaCena;
+            prvaFaktura.tipFakture = tip;
+            prvaFaktura.naziv = naziv;
+            prvaFaktura.cenaPoJediniciMere = cenaPoJediniciMere;
+            prvaFaktura.jedinicaMere = jedinicaMere;
+            prvaFaktura.kolicina = kolicina;
+
+            drugaFaktura.PIBkome = PIBodKoga;
+            drugaFaktura.PIBodKoga = PIBkome;
+            drugaFaktura.datumGenerisanja = DateTime.Now;
+            drugaFaktura.datumPlacanja = datumPlacanja;
+            drugaFaktura.ukupnaCena = ukupnaCena;
+            drugaFaktura.tipFakture = tipDrugeFakture;
+            drugaFaktura.naziv = naziv;
+            drugaFaktura.cenaPoJediniciMere = cenaPoJediniciMere;
+            drugaFaktura.jedinicaMere = jedinicaMere;
+            drugaFaktura.kolicina = kolicina;
+            return Ok(fakture);
+        }
+        //ready
+        [HttpPost("bilans/{PIBZaKoga}")]
+        public IActionResult NekiBilans([FromForm] DateTime pocetak, [FromForm] DateTime kraj,double PIBZaKoga)
+        {   
+            if(fakture.Where(obj => obj.datumPlacanja >= pocetak && obj.datumPlacanja <= kraj)==null || fakture.Where(obj => obj.datumPlacanja >= pocetak && obj.datumPlacanja <= kraj).Any()==false)
+            {
+                return BadRequest("Nema nijedna faktura u datom periodu");
+            };
+            var preduzeceZaKojeJeBilans = preduzeca.FirstOrDefault(obj => obj.PIB == PIBZaKoga);
+            var bilans = fakture.Where(obj => obj.datumPlacanja >= pocetak && obj.datumPlacanja <= kraj && obj.PIBkome == PIBZaKoga);
+            var izlazne = bilans.Where(obj => obj.tipFakture == "izlazna");
+            var ulazne = bilans.Where(obj => obj.tipFakture == "ulazna");
+            var sumaIzlaznih = izlazne.Sum(obj => obj.ukupnaCena);
+            var sumaUlaznih = ulazne.Sum(obj => obj.ukupnaCena);
+            var suma = sumaIzlaznih - sumaUlaznih;
+            return Ok(suma);
+            
+        }
+        //ready
+        [HttpGet("pregledFakturaPoPreduzecu/{PIB}/{page}")]
+        public IActionResult FakturePoPreduzecu(int PIB,int page)
+        {
+            var fakturePoPreduzecu = fakture.Where(obj => obj.PIBkome == PIB).Skip(page*3).Take(3);
+            if (fakturePoPreduzecu.Any() == false)
+            {
+                return BadRequest("Ne postoji faktura / nema dalje");
+            }
+            return Ok(fakturePoPreduzecu);
+        }
+        //ready
+        [HttpGet("pronadjiFakturu/{id}/{PIB}")]
+        public IActionResult PronadjiFakturu(int id,int PIB)
+        {
+            var fakturePoPreduzecu = fakture.FirstOrDefault(obj => obj.PIBkome == PIB && obj.id==id);
+            if (fakturePoPreduzecu==null)
+            {
+                return BadRequest("Ne postoji faktura");
+            }
+            return Ok(fakturePoPreduzecu);
+        }
+        //ready
+        [HttpGet("vratiSvaPreduzeca")]
+        public IActionResult SvaPreduzeca()
+        {
+            var svaPreuduzeca = preduzeca.OrderBy(Preduzece => Preduzece.PIB).ThenBy(Preduzece => Preduzece.naziv);
+            if (svaPreuduzeca.Any() == false)
+            {
+                return NotFound("Nema nijednog preduzeca");
+            }
+            return Ok(svaPreuduzeca);
+        }
+        //ready
+        [HttpGet("vratiSveFakture")]
+        public IActionResult SveFakture()
+        {
+            var sveFakture = fakture;
+            if (sveFakture.Any() == false)
+            {
+                return BadRequest("Nema nijedna faktura");
+            }
+            return Ok(sveFakture);
+        }
+        //ready
+        [HttpPost("PretragaPoNazivuStavkeFakture")]
+        public IActionResult PretragaPoNazivuStavkeFakture([FromForm] string stavka)
+        {
+            var sveFakture = fakture.Where(obj=>obj.naziv ==stavka || obj.jedinicaMere == stavka || obj.cenaPoJediniciMere == Convert.ToDecimal(stavka) || obj.kolicina == Convert.ToDecimal(stavka) || obj.ukupnaCena == Convert.ToDecimal(stavka));
+            if (sveFakture.Any() == false)
+            {
+                return BadRequest("Nema nijedna faktura");
+            }
+            return Ok(sveFakture);
+        }
+        //ready
+        [HttpGet("pronadjiPreduzece/{PIB}")]
+        public IActionResult JednoPreduzece(double PIB)
+        {
+            var nekoPreduzece = preduzeca.FirstOrDefault((p) => p.PIB == PIB);
+            if (nekoPreduzece == null)
+            {
+                return NotFound("Nije pronadjeno preduzece");
+            }
+            return Ok(nekoPreduzece);
+        }
+        //ready
+        [HttpGet("provera/{PIB}")]
+        public IActionResult Provera(double PIB)
+        {
+            var nekoPreduzece = preduzeca.Where((p) => p.PIB == PIB);
+            return Ok(nekoPreduzece);
+        }
+        //ready
+        [HttpGet("filter/{PIB}/{Naziv}")]
+        public IActionResult FilterPreduzece(double PIB,string Naziv)
+        {
+            var filterPIB = preduzeca.Where(enterprise => enterprise.PIB.ToString().Contains(PIB.ToString()));
+            var filterNaziv = preduzeca.Where(preduzeca => preduzeca.naziv.Contains(Naziv));  
+            var filterPreduzeca=preduzeca.Where(preduzeca=> preduzeca.PIB.ToString().Contains(PIB.ToString()) && preduzeca.naziv.Contains(Naziv));
+            if (filterPIB != null && filterNaziv == null)
+            {
+                return Ok(filterPIB);
+            }
+            if(filterPIB == null && filterNaziv != null)
+            {
+                return Ok(filterNaziv);
+            }
+            return Ok(filterPreduzeca.OrderBy(Preduzece => Preduzece.PIB).ThenBy(Preduzece => Preduzece.naziv));
+        }
+        //ready
+        [HttpPost("dodaj")]
+        public IActionResult postovanje([FromForm] string ime, [FromForm] string prezime, [FromForm] string email, [FromForm] string naziv, [FromForm] string adresa, [FromForm] double PIB)
+        {
+            Preduzece preduzece = new Preduzece();
+            preduzece.ime = ime;
+            preduzece.prezime = prezime;
+            preduzece.email = email;
+            preduzece.naziv = naziv;
+            preduzece.adresa = adresa;
+            preduzece.PIB = PIB;
+            if(preduzece == null)
+            {
+                return BadRequest();
+            }
+            if (preduzeca.FirstOrDefault(obj => obj.PIB == preduzece.PIB) != null)
+            {
+                return BadRequest("Vec postoji preduzece sa tim PIB-om");
+            }
+            else
+            {
+                preduzeca.Add(preduzece);
+                return Ok(preduzece);
+            }
+        }
+        //ready
+        [HttpPost("izmeniPreduzece/{PARAMETAR}")]
+        public IActionResult izmeniPreduzece([FromForm] string ime, [FromForm] string prezime, [FromForm] string email, [FromForm] string naziv, [FromForm] string adresa, [FromForm] int PIB, double PARAMETAR)
+        {
+            var obj = preduzeca.FirstOrDefault(x => x.PIB == PARAMETAR);
+            if (obj == null)
+            {
+                return NotFound("Ne postoji preduzece sa tim PIB-om");
+            }
+            if (preduzeca.FirstOrDefault(ness => ness.PIB == PIB) != null && PARAMETAR!=PIB)
+            {
+                return BadRequest("Vec postoji preduzece sa tim PIB-om");
+            }
+            var faktureKome = fakture.Where(obj => obj.PIBkome == PARAMETAR);
+            var faktureOdKoga = fakture.Where(obj => obj.PIBodKoga == PARAMETAR);
+            faktureKome.Select(c => { c.PIBkome = PIB; return c; }).ToList();
+            faktureOdKoga.Select(c => { c.PIBodKoga = PIB; return c; }).ToList();
+            obj.ime = ime;
+            obj.prezime = prezime;
+            obj.email = email;
+            obj.naziv = naziv;
+            obj.adresa = adresa;
+            obj.PIB = PIB;
+            return Ok(obj);
+            
+        }
+
+    }
+    
+}
+
---
+++ API/API/Modals/Faktura.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace faktura.Models
+{
+    
+    public class Faktura
+    {
+        public int id { get; set; }
+        public int PIBkome { get; set; }
+        public int PIBodKoga { get; set; }
+        public DateTime datumGenerisanja { get; set; }
+        public DateTime datumPlacanja { get; set; }
+        public decimal ukupnaCena { get; set; }
+        public string tipFakture { get; set; }
+        public string naziv { get; set; }
+        public decimal cenaPoJediniciMere { get; set; }
+        public string jedinicaMere { get; set; }
+        public int kolicina { get; set; }
+    }
+}
